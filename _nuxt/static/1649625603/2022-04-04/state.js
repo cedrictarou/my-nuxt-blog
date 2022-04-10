a3e7b5e6743d9bbfc2792f48404b3d85feb5d23b
cedrictaro-g <cedric.tarou@gmail.com>
2022-04-11 06:20:05 +0900

    c66d7c1

---
+++ _nuxt/static/1649625603/2022-04-04/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-nuxt-blog\u002F_nuxt\u002Fstatic\u002F1649625603",layout:"default",error:a,serverRendered:true,routePath:"\u002F2022-04-04",config:{_app:{basePath:"\u002Fmy-nuxt-blog\u002F",assetsPath:"\u002Fmy-nuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1f8afe6c"}}}}(null));
